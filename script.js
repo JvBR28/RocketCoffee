2d450d834d1b079744a9bdd2b90095f2196c20ad
JvBR <joaovictor.brodr@gmail.com>
2023-06-22 12:01:13 -0300

    change hamburguer img

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const btnMobile = document.getElementById('btn-mobile');
+const imgButton = document.querySelector('#btn-mobile')
 
 function toggleMenu() {
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
+
+
+  if (nav.classList.contains('active')) {
+    imgButton.setAttribute('src', './assets/menu-buguer-close.svg');
+  }
+  else {
+    imgButton.setAttribute('src', './assets/menu-buguer-open.svg');
+  }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
